3c07c10b7678faf51f9302dfeb4863dc1877c44a
David Elster <davidandtheweb@gmail.com>
2019-10-08 02:47:49 -0400

    Netlify doesn't like empty files.

---
+++ src/templates/category.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '@components/layout'
+import SEO from '@utils/seo'
+
+export default () => (
+  <Layout>
+    <SEO title="Category Archive" />
+    <h1>TODO: Category Page</h1>
+  </Layout>
+)
